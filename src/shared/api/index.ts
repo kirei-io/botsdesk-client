a987585736852f1ef9e0748b557ccbc23261708d
kirei-io <mail.sweetlight@gmail.com>
2023-03-31 22:49:41 +0300

    fix api

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,17 +1,17 @@
 import axios, { type CreateAxiosDefaults } from 'axios'
 
-const authBaseURL = import.meta.env.DEV ? '/auth' : 'https://botsdesk.com/ag'
-const defaultBaseURL = import.meta.env.DEV ? '/api' : 'https://botsdesk.com/cu'
+// const authBaseURL = import.meta.env.DEV ? '/auth' : 'https://botsdesk.com/ag'
+// const defaultBaseURL = import.meta.env.DEV ? '/api' : 'https://botsdesk.com/cu'
 
 const authConfig: CreateAxiosDefaults = {
-  baseURL: authBaseURL,
+  baseURL: 'https://botsdesk.com/ag',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   }
 }
 
 const defaultConfig: CreateAxiosDefaults = {
-  baseURL: defaultBaseURL,
+  baseURL: 'https://botsdesk.com/cu',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem('access_token')}`
