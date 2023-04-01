5d67df5e5aa0902fc6a1e4793b7d6882546b7e9e
Fliksh <fliksh@ro.ru>
2023-04-01 11:55:34 +0400

    add push notifications

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,24 +1,33 @@
 import axios, { type CreateAxiosDefaults } from 'axios'
 
-// const authBaseURL = import.meta.env.DEV ? '/auth' : 'https://botsdesk.com/ag'
-// const defaultBaseURL = import.meta.env.DEV ? '/api' : 'https://botsdesk.com/cu'
+// const authBaseURL = import.meta.env.DEV ? '/auth' : 'https://botsdesk.com/cu'
+// const defaultBaseURL = import.meta.env.DEV ? '/api' : 'https://botsdesk.com/ag'
 
 const authConfig: CreateAxiosDefaults = {
-  baseURL: 'https://botsdesk.com/ag',
+  baseURL: 'https://botsdesk.com/cu',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   }
 }
 
 const defaultConfig: CreateAxiosDefaults = {
-  baseURL: 'https://botsdesk.com/cu',
+  baseURL: 'https://botsdesk.com/ag',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem('access_token')}`
   }
 }
 
+const pushConfig: CreateAxiosDefaults = {
+  baseURL: 'https://botsdesk.com/ph',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('business_access_token')}`
+  }
+}
+
 const AuthApiInstance = axios.create(authConfig)
 const DefaultApiInstance = axios.create(defaultConfig)
+const PushApiInstance = axios.create(pushConfig)
 
-export { AuthApiInstance, DefaultApiInstance }
+export { AuthApiInstance, DefaultApiInstance, PushApiInstance }
